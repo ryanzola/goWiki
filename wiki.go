5e80d0480c78d7dcec8ca32de3001bc8a0a4bfef
ryanzola <ryanzola@me.com>
2018-02-09 17:35:23 -0500

    generating text files successfully. some error handling. cool!

--- wiki.go
+++ wiki.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+)
 
+// Page is a struct for pages
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page")}
+	p1.save()
+	p2, err := loadPage("TestPage")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	} else {
+		fmt.Println(string(p2.Body))
+	}
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
